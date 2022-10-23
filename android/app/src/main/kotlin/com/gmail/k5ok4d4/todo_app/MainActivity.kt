4e8f9977020e3e3b2fe9353516732551fed4dea7
OkadaKeigo <k5ok4d4@gmail.com>
2022-10-23 20:43:54 +0900

    add: first commit

---
+++ android/app/src/main/kotlin/com/gmail/k5ok4d4/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.gmail.k5ok4d4.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Hello World"),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
